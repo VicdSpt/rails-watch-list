bcd298a42c5aa8e39b136898c679b7e179fd2cf0
victor de spirlet <victordespirlet@gmail.com>
2022-11-17 16:56:32 +0100

    dooone

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
-class ReviewsController < ApplicationController
-end
+# class ReviewsController < ApplicationController
+#   def index
+#     @reviews = Reviews.all
+#   end
+
+#   def show
+#     @review = Review.find(params[:id])
+#   end
+  
+#   def new
+#     @review = Review.new
+#   end
+
+#   def create
+#     @review= reviewnew(reviewparams)
+#     if @reviewsave
+#     redirect_to review_path(@review)
+#     else
+#       render:new, status: :unprocessable_entity    
+#    end
+#   end
+
+#   def destroy
+#     @review = Review.find(params[:id])
+#     @review.destroy
+#     redirect_to review_path, status: :see_other
+#   end
+
+#   private
+
+#   def review_params
+#     params.require(:review).permit(:comment, :rating)
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+resources :bookmarks, only: :destroy
 end
